72084713c1118b496486e962759e4fa6f5b2fffa
kwang <kwang@gorillas-MacBook-Pro-7.local>
2018-01-22 21:33:29 -0500

    Delete all except api

--- server.js
+++ server.js
@@ -3,64 +3,15 @@ var fs = require('fs');
 var request = require('request');
 var app = express();
 var path = require('path');
-var parseString = require('xml2js').parseString;
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.get('/player/pksubban', function (req, res) {
-    var url = 'http://www.tsn.ca/mobile/bbcard.aspx?hub=NHL&name=PK+SUBBAN';
-    var json = {};
-    request(url, function (err, response, data) {
-        if (err || response.statusCode != 200) {
-            console.log('REQUEST ERROR: ' + err);
-        }
-        else {
-            parseString(data, function (err, result) {
-                if(!err){
-                    json = JSON.parse(JSON.stringify(result));
-                }
-            });
-        }
-        res.send(json);
-    })
-})
-
-app.get('/player/sheaweber', function (req, res) {
-    var url = 'http://www.tsn.ca/mobile/bbcard.aspx?hub=NHL&name=SHEA+WEBER';
-    var json = {};
-    request(url, function (err, response, data) {
-        if (err || response.statusCode != 200) {
-            console.log('REQUEST ERROR: ' + err);
-        }
-        else {
-            parseString(data, function (err, result) {
-                if(!err){
-                    json = JSON.parse(JSON.stringify(result));
-                }
-            });
-        }
-        res.send(json);
-    })
-})
-
-app.get('/team', function (req, res) {
-    var url = 'http://www.tsn.ca/datafiles/XML/NHL/standings.xml';
-    var json = {};
-    request(url, function (err, response, data) {
-        if (err || response.statusCode != 200) {
-            console.log('REQUEST ERROR: ' + err);
-        }
-        else {
-            parseString(data, function (err, result) {
-                if(!err){
-                   json = JSON.parse(JSON.stringify(result));
-                }
-            });
-        }
-        res.send(json);
-    })
-})
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/poll', function (req, res) {
     var url = 'https://api.polldaddy.com/';
@@ -99,25 +50,12 @@ app.get('/poll', function (req, res) {
     })
 })
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.use(express.static('static'));
-
-app.get('/*', function(req, res) {
-    res.redirect('/');
-});
-
-
 if (process.env.NODE_ENV === 'development') {
     app.listen('8081');
 }
 if (process.env.NODE_ENV === 'production') {
     app.listen(server_port, server_ip_address, function () {
-
         console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
-
     });
 }
 
