5cba6c0ae54a9f8ad8d36de2ebc05468130a92bb
Kai <kaichen.wang@outlook.com>
2026-06-29 16:33:08 -0400

    Remove FB comments (API for social plugins is deprecated)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import { SEASONS, LATEST_SEASON_ID } from './common/data';
@@ -8,7 +8,6 @@ import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
 
 import Stats from './components/Stats';
-import Comments from './components/Comments';
 import Select from './components/Select';
 
 /* Google Analytics */
@@ -23,16 +22,9 @@ history.listen((location) => {
 function App() {
   const [seasons, setSeasons] = useState(SEASONS);
   const [seasonId, setSeasonId] = useState();
-  const [commentsActive, setCommentsActive] = useState();
-
-  const buttonComments = createRef();
 
   const handleChange = (evt) => {
     setSeasonId(evt.currentTarget.value);
-  };
-
-  const handleClick = () => {
-    setCommentsActive(!commentsActive);
   };
 
   const handleError = () => {
@@ -47,7 +39,6 @@ function App() {
   const location = useLocation();
   const paramsPrevious = usePrevious(new URLSearchParams(location.search));
   const seasonIdPrevious = usePrevious(seasonId);
-  const commentsActivePrevious = usePrevious(commentsActive);
 
   useEffect(() => {
     /* Sync seasonId with param */
@@ -83,37 +74,6 @@ function App() {
     }
   }, [paramsPrevious, history, seasonId, seasonIdPrevious]);
 
-  useEffect(() => {
-    /* Sync commentsActive with param */
-    const params = new URLSearchParams(history.location.search);
-    const paramCurrent = params.get('comments') === 'true';
-    const paramPrevious = paramsPrevious
-      ? paramsPrevious.get('comments') === 'true'
-      : !paramCurrent;
-    const stateCurrent = commentsActive;
-    const statePrevious =
-      typeof commentsActivePrevious === 'undefined'
-        ? stateCurrent
-        : commentsActivePrevious;
-    const type = getUpdateType(
-      paramPrevious,
-      paramCurrent,
-      statePrevious,
-      stateCurrent
-    );
-    if (type === 'state') {
-      setCommentsActive(paramCurrent);
-    } else if (type === 'param') {
-      if (stateCurrent) {
-        params.set('comments', true);
-        history.push({ search: params.toString() });
-      } else {
-        params.delete('comments');
-        history.replace({ search: params.toString() });
-      }
-    }
-  }, [paramsPrevious, history, commentsActive, commentsActivePrevious]);
-
   return (
     <div className="App">
       <main className="App__main">
@@ -133,27 +93,7 @@ function App() {
         <label className="visually-hidden" htmlFor="SeasonId">
           Select season
         </label>
-        <button
-          aria-label="Open discussion"
-          onClick={handleClick}
-          ref={buttonComments}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1.8rem"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-          >
-            <path d="M22 3v13h-11.643l-4.357 3.105v-3.105h-4v-13h20zm2-2h-24v16.981h4v5.019l7-5.019h13v-16.981zm-5 6h-14v-1h14v1zm0 2h-14v1h14v-1zm-6 3h-8v1h8v-1z" />
-          </svg>
-          <span aria-hidden="true">Discussion</span>
-        </button>
       </div>
-      <Comments
-        commentsActive={commentsActive}
-        setCommentsActive={setCommentsActive}
-        setFocus={buttonComments}
-      />
     </div>
   );
 }
